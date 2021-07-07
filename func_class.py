5073c91523a66d71cbe86535362b6de2ce0348c1
demka06 <59875707+demka06@users.noreply.github.com>
2021-07-07 22:03:54 +0300

    Update func_class.py

--- func_class.py
+++ func_class.py
@@ -892,7 +892,7 @@ class Main(object):
 						)
 		else:
 			curs.execute(f"SELECT race_id FROM users WHERE user_id = {self.user_id}")
-			if curs.fetchone( )[0] == 0:
+			if curs.fetchone( )[0] == 1:
 				race = self.command.split(" ")[0]
 				if race.isdigit( ):
 					curs.execute("SELECT race_id FROM races ORDER BY race_id DESC LIMIT 1")
