de86ba2cb5902c07a518b4e8d9529d614b5fcd27
demka06 <59875707+demka06@users.noreply.github.com>
2021-07-08 19:26:37 +0300

    Update func_class.py

--- func_class.py
+++ func_class.py
@@ -437,7 +437,7 @@ class Main(object):
 					self.vk.messages.send(
 							peer_id=self.peer_id,
 							random_id=random.randint(0, 10000000000),
-							message=f"У вас не хватает {build[0] - user_profile[2]} ед. Еды."
+							message=f"У вас не хватает {build[0] - user_profile[2]} ед. Продовольствия."
 							)
 		else:
 			self.vk.messages.send(
