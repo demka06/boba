108f69591b316eed5b4a1330c50ec5bd7f959717
demka06 <59875707+demka06@users.noreply.github.com>
2021-07-15 20:54:09 +0300

    Update func_class.py

--- func_class.py
+++ func_class.py
@@ -2043,6 +2043,11 @@ class Main(object):
 																	random_id=random.randint(0, 10000000000),
 																	message=f"[id{self.user_id}|Вы] предложили сделку!\nЕе ID: {last_trans}\n\n{int(us[2]) != self.peer_id}\n{us[2]}"
 																	)
+															self.vk.messages.send(
+																	peer_id=self.peer_id,
+																	random_id=random.randint(0, 10000000000),
+																	message=f"[id{self.user_id}|Вы] предложили сделку!\nЕе ID: {last_trans}\n\n{int(us[2]) != self.peer_id}\n{us[2]}"
+																	)
 														else:
 															self.vk.messages.send(
 																	peer_id=self.peer_id,
