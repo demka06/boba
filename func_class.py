7e7e16e60f6d9acc7477641a44d1f153f38feabe
demka06 <59875707+demka06@users.noreply.github.com>
2021-07-15 11:07:53 +0300

    Update func_class.py

--- func_class.py
+++ func_class.py
@@ -1339,9 +1339,7 @@ class Main(object):
 			curs = conn.cursor( )
 			curs.execute(f'SELECT verif FROM conversations WHERE peer_id = {self.peer_id}')
 			a = curs.fetchone( )[0]
-			print(a)
 			if int(a) == 1:
-				print(a)
 				now_utc = datetime.now(timezone('UTC'))
 				time = now_utc.astimezone(timezone('Europe/Moscow'))
 				curs.execute(f"SELECT COUNT(lot_id) FROM market WHERE from_user = {self.user_id}")
