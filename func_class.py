af63c2636a3da391efc84c5904b3153484dfbded
demka06 <59875707+demka06@users.noreply.github.com>
2021-07-16 23:29:54 +0300

    Update func_class.py

--- func_class.py
+++ func_class.py
@@ -2350,7 +2350,7 @@ class Main(object):
 		ch = int(curs.fetchone( )[0])
 		if ch == 1:
 			curs.execute(
-					f"SELECT to_user, res_id, count, cost, trans_id FROM personal_trans WHERE rej = 0 and to_user = {self.user_id} and purch = 0"
+					f"SELECT from_user, res_id, count, cost, trans_id FROM personal_trans WHERE rej = 0 and to_user = {self.user_id} and purch = 0"
 					)
 			from_you = curs.fetchall( )
 			fr = ""
@@ -2359,7 +2359,7 @@ class Main(object):
 				fr += f"ID: #{i[4]}\nДля кого: @id{i[0]}\nРесурс: {curs.fetchone( )[0]}\nКол-во: {i[2]}\nСтоимость: {i[3]}\n\n"
 			
 			curs.execute(
-					f"SELECT from_user, res_id, count, cost, trans_id FROM personal_trans WHERE rej = 0 and from_user = {self.user_id} and purch = 0"
+					f"SELECT to_user, res_id, count, cost, trans_id FROM personal_trans WHERE rej = 0 and from_user = {self.user_id} and purch = 0"
 					)
 			for_you = curs.fetchall( )
 			fro = ""
